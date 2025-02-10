1bedc1cb4eaf90b36e9585fe7e673af3cfd20341
Sherzod <you@example.com>
2025-02-10 21:32:03 +0500

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import { ChildProps } from '@/types'
+import React from 'react'
+import Navbar from './_components/navbar'
+import Footer from './_components/footer'
+
+const Layout = ({ children }: ChildProps) => {
+	return (
+		<main>
+			<Navbar />
+			<div className='container'>{children}</div>
+			<Footer />
+		</main>
+	)
+}
+
+export default Layout
